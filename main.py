f19229248647fe704a88bfa06b6a23a46a5fc4f5
Maule Alessandro <maule2703@gmail.com>
2021-12-23 12:56:00 +0100

    1

--- main.py
+++ main.py
@@ -2,8 +2,7 @@ import logging
 import pyrogram 
 import json # for config
 import requests
-from pyrogram import Update
-from pyrogram import Updater, CommandHandler, MessageHandler, Filters
+from pyrogram import 
 # Amazon Stuff
 from amazon.tools import get_asin # Get asin from url
 from utils.create_message import amazon_message # Create HTML template for amazon
