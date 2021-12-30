2b51d945f60689e1e5917ee6950d755341059c3f
Falcone8 <maule2703@gmail.com>
2021-12-30 09:34:09 +0100

    Definitive Commit

--- utils/tools.py
+++ utils/tools.py
@@ -4,4 +4,3 @@ def check_domain(url):
     return re.search(r'(?:[a-z0-9](?:[a-z0-9-]{0,61}[a-z0-9])?\.)+[a-z0-9][a-z0-9-]{0,61}[a-z0-9][/]', url).group(0)
 def get_asin(text):
     return re.findall(r'/(?:dp|o|gp|-)\/(B[0-9]{2}[0-9A-Z]{7}|[0-9]{9}(?:X|[0-9]))/', text)[0]
-
